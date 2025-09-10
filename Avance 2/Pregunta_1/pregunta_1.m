cb0e870e13e5951244555ecf514d813f25bada03
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-09-10 15:49:10 -0600

    teorema de unicidad implementado
    En teoria, aparte de leves correciones, la pregunta 1 esta lista

--- Avance 2/Pregunta_1/pregunta_1.m
+++ Avance 2/Pregunta_1/pregunta_1.m
@@ -22,7 +22,9 @@ function c = unicidad_pf(f,a,b)
 
   fs=sym(f);
   fds=diff(fs);
+  fds2=diff(fs,2);
   fdn=matlabFunction(fds);  %Funcion Numerica de derivada (Usar)
+  fdn2=matlabFunction(fds2);%Funcion Numerica de 2da derivada (Usar)
 
   %Teorema de Existencia
 
@@ -31,56 +33,39 @@ function c = unicidad_pf(f,a,b)
   xsol=fzero(fdn,[a,b]);
 
 
-  %Evaluar puntos criticos y exttremos del intervalo en h(x)
-  crit = fn(xsol)
-  fa = fn(a)
-  fb = fn(b)
+  %Evaluar puntos criticos y extremos del intervalo en h(x)
+  crit = fn(xsol);
+  fa = fn(a);
+  fb = fn(b);
+
+  %Verificar que fa y fb estén en el intervalo [a,b]
 
   if fa >= a && fa <= b && fb >= a && fb <= b
-    fprintf('La función tiene al menos un punto fijo en [a,b] \n');
+    fprintf('f(x) tiene al menos un punto fijo en [a,b] \n');
     existencia = true;
   else
-    fprintf('No se cumple el teorema de existencia');
+    fprintf('No se cumple el teorema de existencia \n');
     existencia = false;
   endif
 
   %Teorema de Unicidad
 
   if existencia
+    xsol2=fzero(fdn2,[a,b]);
 
+    critfd = fdn(xsol2);
+    fda = fdn(a);
+    fdb = fdn(b);
+
+    if fda >= -1 && fda <= 1 && fdb >= -1 && fdb <= 1
+      unicidad = 1;
+      fprintf("f(x) tiene un único punto fijo en el intervalo [a,b] \n")
+    else
+      unicidad = 0;
+    endif
+  else
+    fprintf("No se cumple el teorema de existencia \n")
   endif
 
-
-  %xd = 6<8;
-  %if xd == 1;
-    %printf("hola");
-  %else
-    %printf("adios");
-  %endif;
-
-
-  c = 1;
-
+  c = unicidad
 end
-
-
-%Ejemplo 1: 'fminsearch' = Calcular  el mínimo de una función f, dado un valor inicial.
-%x0=50;
-%xmin=fminsearch(f1,x0);
-%ymin=f1(xmin);
-
-%Ejemplo 2: 'fzero' = Calcular una solución de la ecuación f(x)=0, dado un valor inicial
-%x0=-3;
-%xsol=fzero(f1,x0);
-
-%Ejemplo3: 'fminbnd' = Calcular el mínimo de una función f en un intervalo [a,b]
-%a=-4;
-%b=-2;
-%xmin=fminbnd(f1,a,b);
-
-%Ejercicio: Calcular el máximo de una funcion f en un intervalo [a,b]
-%a=1;
-%b=3;
-%newf1=@(x) -1*f1(x); %Reflejar f1 en el eje x
-%fmaxbnd=@(newf1,a,b) fminbnd(newf1,a,b);
-%xmax=fmaxbnd(newf1,a,b)
